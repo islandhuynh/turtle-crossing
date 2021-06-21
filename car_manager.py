a1a792515ddd2465a6f253bf96c0e41bb66cb085
Island <islandhuynh@gmail.com>
2021-06-21 05:00:09 -0400

    added a class which generates randomly moving cars of different colors

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-
 class CarManager:
-    pass
+    def __init__(self):
+        self.cars = []
+    
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.color(COLORS[random.randint(0, len(COLORS) - 1)])
+        new_car.penup()
+        new_car.shapesize(1, 2, 1)
+        new_car.goto(x = 300, y = random.randint(-250, 250))
+        self.cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.setx(car.xcor() - MOVE_INCREMENT )
--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import time
+import random
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+
+FINISH_LINE_Y = 280
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player_icon = Player()
 level = Scoreboard()
+cars = CarManager()
 
 screen.onkeypress(player_icon.move, "Up")
 screen.listen()
 
 game_is_on = True
 while game_is_on:
+    random_int = random.randint(1, 5)
+    if random_int == 5:
+        cars.create_car()
+    cars.move_cars()
     time.sleep(0.1)
     screen.update()
     
